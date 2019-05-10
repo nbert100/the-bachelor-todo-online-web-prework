e90bffd8c771bc8087606e27b61848dd2b59ec58
Nia Bert <nbert14@gmail.com>
2019-05-10 00:44:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season, contestant| 
+    if contestant[status] == "Winner"
+      return contestant[name].split[0]
+    end
+       end
 end
 
 def get_contestant_name(data, occupation)
@@ -11,7 +15,9 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do 
+    return
 end
 
 def get_average_age_for_season(data, season)
