93c858c3672b8a627b1b977ecf7b435b058a7e67
Nia Bert <nbert14@gmail.com>
2019-05-10 00:46:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,11 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |season, contestant| 
-    if contestant[status] == "Winner"
+    "status" = :status
+    "name" = :name
+    if contestant[:status] == "Winner"
       return contestant[name].split[0]
     end
-  
+  end
 end
 
 def get_contestant_name(data, occupation)
